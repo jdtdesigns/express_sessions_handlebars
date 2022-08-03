997d5121c92470bb2e278889c474c951577de326
JD Tadlock <jt7903@gmail.com>
2022-08-03 09:55:14 -0400

    session setup complete

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const { engine } = require('express-handlebars');
 const PORT = process.env.PORT || 3333;
 // Pull in our sequelize connection object
 const db = require('./config/db_connection');
+// Package that allows us to attach sessions to the server - temp storage
+const session = require('express-session');
+// Allows us to store our session data to the database instead of using system memory
+// This is extremely important for any app that will have many users logging in/out
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 // attach .env to process object
 require('dotenv').config();
 // Pull in our view routes object from the index file
@@ -22,6 +28,23 @@ app.set('view engine', 'hbs');
 app.use(express.json());
 // Allow form data to be sent through and also allow object/array data - req.body
 app.use(express.urlencoded({ extended: false }));
+
+// Add session middleware to the server - gives us access to req.session on our routes
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  // Stores our session data to the database instead of using server system memory
+  store: new SequelizeStore({ db }),
+  // If we don't affect/change the session data during a request, this option
+  // will allow the the store to "forget" the session at the end of a request
+  saveUninitialized: false,
+  // Keeps the sequelize store from deleting idle session data
+  resave: false,
+  // The cookie object allows us to manipulate the client-side cookie - set expiration,
+  // set to httpOnly(is not accessible to client JS), etc.
+  cookie: {
+    // httpOnly: true
+  }
+}));
 
 // Load our view routes at the root route - localhost:3333/
 app.use('/', view_routes);
