b208cbad0e7fc116a3b71cfe541c16caccdb083b
JD Tadlock <jt7903@gmail.com>
2022-08-03 01:10:25 -0400

    added hbs helper code to index

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -5,7 +5,12 @@ const view_router = require('express').Router();
 view_router.get('/', (req, res) => {
   // Sends our our index.hbs file back to the client-side - main.hbs is loaded first
   // then anything inside of index.hbs is outputted through the {{{body}}}
-  res.render('index');
+  res.render('index', {
+    fruits: ['apple', 'orange', 'grape'],
+    user: {
+      email: 'test@test.com'
+    }
+  });
 });
 
 module.exports = view_router;
