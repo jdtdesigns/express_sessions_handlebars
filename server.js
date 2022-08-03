dd7e5e99aa3d574689fe901b2076705ce36f5841
JD Tadlock <jt7903@gmail.com>
2022-08-03 00:05:58 -0400

    added handlebars

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const path = require('path');
+const { engine } = require('express-handlebars');
 const PORT = process.env.PORT || 3333;
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.static(path.join('front')));
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
