e667e3df9dde908fcb160642bcfd3dfa243e785a
JD Tadlock <jt7903@gmail.com>
2022-08-03 01:23:57 -0400

    added all views

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -3,14 +3,18 @@ const view_router = require('express').Router();
 
 // GET route listening on localhost:3333/
 view_router.get('/', (req, res) => {
+
   // Sends our our index.hbs file back to the client-side - main.hbs is loaded first
   // then anything inside of index.hbs is outputted through the {{{body}}}
-  res.render('index', {
-    fruits: ['apple', 'orange', 'grape'],
-    user: {
-      email: 'test@test.com'
-    }
-  });
+  res.render('index');
 });
+
+view_router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+view_router.get('/register', (req, res) => {
+  res.render('register');
+})
 
 module.exports = view_router;
